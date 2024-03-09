86dd188eb8e05d94416af580a9bd8a4c0174c70a
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-08 16:07:24 -0800

    venv issue, file rewritten

--- get_json/json_practice.py
+++ get_json/json_practice.py
@@ -1,14 +1,26 @@
 """Write vanilla flask
-1. install python requests with pip3 install requests
-2.
+1. VENVS!!!! GRACE THINK!!! ENVIRONMENT THEN CODE!!!
+2. install python requests with pip3 install requests
 3.
 4.
 """
 import requests
 
+TVMAZE_URL = "https://api.tvmaze.com/search/shows?q=startrek"
+JSON_TO_MESS_WITH = None
+
 def make_req():
     """Makes a get request to the API and returns in json and stores it in
-    the global scop."""
+    the global scope."""
+    try:
+        resp = requests.get(TVMAZE_URL)
+        json = resp.json()
+
+        global JSON_TO_MESS_WITH
+        JSON_TO_MESS_WITH = json
+        
+    except ValueError:
+        return "Issue with request or JSON"
 
 
 def find_first():
