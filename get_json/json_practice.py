03e33dc9f35b6c777334c463715433b14eae0b00
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-08 16:17:26 -0800

    vanilla python API request

--- get_json/json_practice.py
+++ get_json/json_practice.py
@@ -7,18 +7,15 @@
 import requests
 
 TVMAZE_URL = "https://api.tvmaze.com/search/shows?q=startrek"
-JSON_TO_MESS_WITH = None
 
 def make_req():
-    """Makes a get request to the API and returns in json and stores it in
-    the global scope."""
+    """Makes a get request to the API and returns in json."""
+
     try:
         resp = requests.get(TVMAZE_URL)
         json = resp.json()
+        return json
 
-        global JSON_TO_MESS_WITH
-        JSON_TO_MESS_WITH = json
-        
     except ValueError:
         return "Issue with request or JSON"
 
