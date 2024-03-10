bf9dccd9955dfefaeef68630333d97878feec5fe
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-10 11:46:37 -0700

    practice methods

--- get_json/json_practice.py
+++ get_json/json_practice.py
@@ -7,21 +7,48 @@
 import requests
 
 TVMAZE_URL = "https://api.tvmaze.com/search/shows?q=startrek"
+URL = "fakeURL.com"
 
 def make_req():
     """Makes a get request to the API and returns in json."""
 
     try:
-        resp = requests.get(TVMAZE_URL)
+        resp = requests.get(URL)
         json = resp.json()
         return json
 
     except ValueError:
-        return "Issue with request or JSON"
+        raise ValueError('Error in request process')
 
 
 def find_first():
-    """Takes JSON and returns the first ___ value."""
+    """Takes JSON and returns the first show value."""
+
+    json = make_req()
+    return json[0]
+
 
 def collect_names():
-    """Takes JSON and returns an array of all ___ names."""
+    """Takes JSON and returns list of all show names."""
+
+    names = []
+    json = make_req()
+
+    print(json)
+    for show_info in json:
+        names.append(show_info["show"]["name"])
+
+    return names
+
+
+""" Get JSON from a file."""
+
+def read_file(file_path, search_term):
+    """Takes a file and reads it. Collects all information base
+    on provided search term."""
+
+    try:
+        json = open(file_path)
+        print(json)
+    except FileNotFoundError:
+        raise FileNotFoundError()
