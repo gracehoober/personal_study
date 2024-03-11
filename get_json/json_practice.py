705336273b6b9046b3af523b515bdf48ba459188
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-11 14:06:46 -0700

    flatten function

--- get_json/json_practice.py
+++ get_json/json_practice.py
@@ -49,12 +49,32 @@ import json
 def empolyees_search(file_path, search_term):
     """Takes a file and reads it. Collects all people base
     on provided search term."""
+    """ look through whole nested structure
+        flatten out structure... like all i need is a list of values...
+        iterate over the flattened structure for a like item
+    """
 
+def flatten(data):
+    """Takes JSON structure and returns a list of all values in the structure
+        like: [{'cat': 'james', 'dog': 'marty'}, {'cow': 'spot'}]
+                        -> ['cat', 'james', 'dog', 'marty', 'cow', 'spot']
+    """
+    stack = [data]
+    flattened =[]
 
+    while(len(stack) > 0):
+        last = len(stack) - 1
+        item = stack[last]
 
+        if isinstance(item,list) is False and isinstance(item,dict) is False:
+            flattened.append(item)
+        else:
+            for val in item:
+                stack.append(val)
 
+        stack.pop()
 
-
+    return set(flattened)
 
 def all_projets(file_path):
     """Returns a list of projects"""
